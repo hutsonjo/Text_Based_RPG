1d59793363a9f4a0be0039c214e16a2429dd7243
hutsonjo <hutsonjo@oregonstate.edu>
2025-11-02 20:20:56 -0500

    Formatted the UI for the gameplay once a session has been initiated

--- UI.py
+++ UI.py
@@ -86,16 +86,16 @@ class GameLogic:
 
     def __init__(self):
         """Initialize the game instance, defined by player character save data and other factors."""
-        self._player = Player({"name": "placeholder",
-                           "stats": {
-                               "health": 100,
-                               "mana": 0,
-                               "attack": 10,
-                               "defense": 10
-                           },
-                           "inventory": [],
-                           "position": ["placeholder", [0, 0]]
-                           })
+        self._player = Player({
+            "name": "placeholder",
+            "stats": {
+                "health": 100,
+                "mana": 0,
+                "attack": 10,
+                "defense": 10
+            },
+            "inventory": [],
+            "position": ["placeholder", [0, 0]]})
 
     def load_player(self):
         """Access the save_file.json file get information to create a previously saved player object."""
@@ -131,36 +131,44 @@ class UI:
         self._root.option_add("*Foreground", "white")
         self._root.option_add("*Background", "#171717")
 
-        # UI component frames
-        self._control_panel = Frame(self._root, bg='#9c9c9c')
-        self._direction_frame = Frame(self._control_panel, bg='#9c9c9c')
-        self._inventory_frame = Frame(self._control_panel, bg='#9c9c9c')
-        self._inspect_frame = Frame(self._control_panel, bg='#9c9c9c')
-        self._stats_frame = Frame(self._control_panel, bg='#9c9c9c')
+        # UI control component frames
+        self._direction_frame = Frame(self._root, bg='#9c9c9c')
+        self._inventory_frame = Frame(self._root, bg='#9c9c9c')
+        self._inspect_frame = Frame(self._root, bg='#9c9c9c')
+        self._stats_frame = Frame(self._root, bg='#9c9c9c')
+
+        # UI control component buttons
+        self._north_button = Button(self._direction_frame, text='North', command=lambda: self._move('north'))
+        self._east_button = Button(self._direction_frame, text='East', command=lambda: self._move('east'))
+        self._south_button = Button(self._direction_frame, text='South', command=lambda: self._move('south'))
+        self._west_button = Button(self._direction_frame, text='West', command=lambda: self._move('west'))
+        self._inventory_button = Button(self._inventory_frame, text='Inventory', command=self._inventory_page)
+        self._inspect_button = Button(self._inspect_frame, text='Inspect', command=self._inspect_page)
+        self._stats_button = Button(self._stats_frame, text='Stats', command=self._stats_page)
 
         # Start window frames and button
         self._text_window = Frame(self._root)
         self._text_window.place(relx=0, rely=0, relwidth=1, relheight=.8)
-        self._intro_label = Label(self._text_window,
-                                  text=INTRO_TEXT,
-                                  wraplength=800,
-                                  justify='center',
-                                  )
-        self._intro_label.pack(expand=True, fill='both')
-        self._intro_bottom_frame = Frame(self._root)
-        self._intro_bottom_frame.place(relx=0, rely=0.8, relwidth=1, relheight=.2)
-        self._ready_button = Button(self._intro_bottom_frame,
+        self._text_label = Label(self._text_window,
+                                 text=INTRO_TEXT,
+                                 wraplength=800,
+                                 justify='center',)
+        self._text_label.pack(expand=True, fill='both')
+        self._bottom_window = Frame(self._root)
+        self._bottom_window.place(relx=0, rely=0.8, relwidth=1, relheight=.2)
+        self._ready_button = Button(self._bottom_window,
                                     text="I'm Ready!",
                                     command=self._ready,
                                     bg='#9c9c9c')
         self._ready_button.place(relx=0.5, rely=0.25, anchor='center')
+        self._help_button = Button(self._bottom_window, text='Help', command=self._help_page)  # Later use
 
         # Load window buttons
-        self._new_file_button = Button(self._intro_bottom_frame,
+        self._new_file_button = Button(self._bottom_window,
                                        text="New File",
-                                       command=lambda: self._initiate_game,
+                                       command=self._initiate_game,
                                        bg='#9c9c9c')
-        self._load_button = Button(self._intro_bottom_frame,
+        self._load_button = Button(self._bottom_window,
                                    text="Continue",
                                    command=self._load_save,
                                    bg='#9c9c9c')
@@ -170,7 +178,7 @@ class UI:
 
     def _ready(self):
         """Responds user clicking the ready button and moves to the next page offering the chance to load save data"""
-        self._intro_label.config(text=LOAD_TEXT)
+        self._text_label.config(text=LOAD_TEXT)
         self._ready_button.place_forget()
         self._new_file_button.place(relx=0.4, rely=0.25, anchor="center")
         self._load_button.place(relx=0.6, rely=0.25, anchor="center")
@@ -182,7 +190,45 @@ class UI:
 
     def _initiate_game(self):
         """Initiates the gameplay portion of the UI."""
-        # Reshape the text window
+        # Reshape the text windows
+        self._text_window.place_configure(relx=0, rely=0, relwidth=.8, relheight=.8)
+        self._bottom_window.place_configure(relx=0, rely=0.8, relwidth=.8, relheight=.2)
+
+        # Place the frames for the controls onto the UI
+        self._direction_frame.place(relx=0.8, rely=0, relwidth=.2, relheight=.25)
+        self._inventory_frame.place(relx=0.8, rely=0.25, relwidth=.2, relheight=.25)
+        self._inspect_frame.place(relx=0.8, rely=0.5, relwidth=.2, relheight=.25)
+        self._stats_frame.place(relx=0.8, rely=0.75, relwidth=.2, relheight=.25)
+
+        # Place the buttons into the control frames
+        self._north_button.place(relx=0.5, rely=0.0, anchor="n")
+        self._east_button.place(relx=1, rely=0.5, anchor="e")
+        self._south_button.place(relx=0.5, rely=1, anchor="s")
+        self._west_button.place(relx=0, rely=0.5, anchor="w")
+        self._inventory_button.pack(expand=True, fill="both")
+        self._inspect_button.pack(expand=True, fill="both")
+        self._stats_button.pack(expand=True, fill="both")
+        self._help_button.pack(side='bottom', anchor='w')
+
+    def _move(self, direction):
+        """Advances the player through the map"""
+        pass
+
+    def _inventory_page(self):
+        """Restructures the text window according to the inventory of the player character."""
+        pass
+
+    def _inspect_page(self):
+        """Inspects the environment, fetching additional text information for the user to read."""
+        pass
+
+    def _stats_page(self):
+        """Restructures the text window according to the stats of the player character."""
+        pass
+
+    def _help_page(self):
+        """Deploys a pop-up window to remind the player of the control scheme."""
+        pass
 
 
 if __name__ == '__main__':
